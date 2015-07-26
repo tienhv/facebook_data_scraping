812ae268a430456d8c3b1d20702ebde6406f3883
Steven Vo <findme@stevenvo.info>
2015-07-25 23:38:23 -0700

    init

---
+++ facebook_data_scraping/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for facebook_data_scraping project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'facebook_data_scraping'
+
+SPIDER_MODULES = ['facebook_data_scraping.spiders']
+NEWSPIDER_MODULE = 'facebook_data_scraping.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'facebook_data_scraping (+http://www.yourdomain.com)'
